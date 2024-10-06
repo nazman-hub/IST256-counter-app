e5b7f53ccbd47f82971a12ccc88977b0f9cf2555
nazman-hub <123609252+nazman-hub@users.noreply.github.com>
2024-10-06 14:55:00 -0400

    finsih

--- src/counter-app.js
+++ src/counter-app.js
@@ -10,23 +10,57 @@ export class counterApp extends DDDSuper(LitElement) {
   constructor() {
     super();
     this.title = "Counter";
-    this.count = 0;
-    this.min = 0;
-    this.count = 0;
+    this.min = 20;
+    this.count = this.min;
+    this.max = 25;
+    this.fancy=false;
   }
 
   static get properties() {
     return {
       title: { type: String },
-      count: { type: Number }
-    };
-  }
+      count: { type: Number },
+      min: { type: Number },
+      max: { type: Number },
+      fancy: { type: Boolean, reflect: true }  
+    }
+  };
 
   increaseCount(){
     this.count+=1;
   }
   decreaseCount(){
     this.count-=1;
+    console.log(this.min, this.count)
+  }
+  updated(e){
+    if(this.count === 21){
+      this.fancy = true;
+      this.makeItRain();
+    } else{
+      this.fancy =false;
+    }
+  }
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
   }
 
 
@@ -39,47 +73,82 @@ export class counterApp extends DDDSuper(LitElement) {
         color: black;
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
-        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
+        font-size: var(--counter-app-font-size, var(--ddd-font-size-l));
+        
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-        background-color: lightblue;
-        width: 300px;
-        display: flex;
-        gap: 20px;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      div {
+      div, h1 {
         padding: 0;
         margin: 0;
       }
+      .counter-wrapper {
+        padding: var(--ddd-spacing-10);
+        background-color: var(--ddd-theme-default-white);
+        display: flex;
+        gap: var(--ddd-spacing-4);
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: var(--ddd-border-lg);
+        border-color: var(--ddd-theme-default-beaverBlue);
+      }
+ 
+      .confetti-wrapper{
+        padding: var(--ddd-spacing-4);
+        width: 400px;
+      }
+
+      .count{
+        font-size: 128px;
+        
+      }
+
+      :host([fancy]) .count{
+        color: blue;
+      }
+
       .count-buttons button{
         width:100px;
         height: 100px;
-        margin: 16px;
+        /* margin: 16px; */
         font-size: 24px; 
+        background-color: var(--ddd-theme-default-beaverBlue);
+        color: white;
+        font-size: var(--ddd-font-size-l);
       }
+      .count-buttons button:hover{
+        cursor:pointer;
+        background-color: var(--ddd-theme-default-beaver80);
+        
+      }
+      .count-buttons button:disabled{
+        cursor:default;
+        background-color: var(--ddd-theme-default-navy40);
+        
+      }
+      
 
     `];
   }
 
   render() {
     return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  <div class="count">
-   ${this.count}
-  </div>
-  <div class="count-buttons">
-     <button @click="${this.decreaseCount}">-</button>
-    <button @click="${this.increaseCount}">+</button>
-    
-  </div>
-  <slot></slot>
-</div>`;
+    <div class="confetti-wrapper">
+      <confetti-container id="confetti">
+        <div class="counter-wrapper">
+
+          <div>${this.title}</div>
+          <h1 class="count">${this.count}</h1>
+          <div class="count-buttons">
+            <button @click="${this.decreaseCount}" ?disabled="${this.min === this.count}">-</button>
+            <button @click="${this.increaseCount}" ?disabled="${this.max === this.count}">+</button>
+          </div>
+          <slot></slot>
+        </div>
+       </confetti-container>
+    </div>
+
+
+  `;
   }
 
   /**
